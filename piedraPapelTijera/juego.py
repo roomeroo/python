e6e05b52dbf6335aaa6415b00d0c25ab508ff827
roomeroo05 <roomeroo05@gmail.com>
2025-02-26 12:04:50 +0100

    piedra-papel-tijeras

--- piedraPapelTijera/juego.py
+++ piedraPapelTijera/juego.py
@@ -1,20 +1,46 @@
 import random
+
+#Definir colores
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+RESET = "\033[0m"
+
 juego = {"piedra", "papel", "tijera"}
 
 def pedirInput():
-  buenaEntrada = False
   while(True):
-    entrada = input("Piedra papel o tijera: ").lower
-    if(entrada in str):
+    entrada = input("Piedra papel o tijera: ").lower()
+    if(entrada in juego):
       return (entrada)
+    else: print("Error. Intenta de nuevo")
   
 
 def jugarRonda():
   usuario = pedirInput()
-  maquina = random.choice(juego)
-  if(maquina == usuario): return {0,0}
-  elif(maquina == "piedra" and usuario == "tijera"): return{0,1}
-  elif(maquina == "piedra" and usuario == "papel"): return{1,0}
-  elif(maquina == "papel" and usuario == "tijera"): return{0,1}
-  elif(maquina == "papel" and usuario == "piedra"): return{1,0}
-  elif(maquina == "tijera" and usuario == "piedra"): return{}
+  maquina = random.choice(list(juego))
+  print(YELLOW + maquina + RESET)
+  if(maquina == usuario): return 0,0
+  elif(maquina == "piedra" and usuario == "tijera"): return 0,1
+  elif(maquina == "papel" and usuario == "tijera"): return 0,1
+  elif(maquina == "tijera" and usuario == "piedra"): return 0,1
+  else: return 1,0
+  
+
+numeroRondas = 0
+puntuacionUsuario = 0
+puntuacionMaquina = 0
+
+while(numeroRondas < 3):
+  numeroRondas += 1
+  puntuacion = jugarRonda()
+  puntuacionUsuario += puntuacion[1]
+  puntuacionMaquina += puntuacion[0]
+  if(int(puntuacion[1]) > 0): print(GREEN +"Ronda para el usuario." + RESET)
+  elif(puntuacion[1] == puntuacion[0]): 
+    print(YELLOW + "empate, esta ronda no cuenta" + RESET) 
+    numeroRondas -= 1
+  else: print(RED + "Ronda para la máquina" + RESET)
+  print(f"Puntuaciones:\nUsuario {puntuacionUsuario}\nMáquina {puntuacionMaquina}")
+if(puntuacionUsuario > puntuacionMaquina): print(GREEN + "Ha ganado el usuario!" + RESET)
+else: print(RED + "Ha ganado la máquina!" + RESET)
